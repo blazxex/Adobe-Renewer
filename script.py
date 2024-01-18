16f060c761c8bc6305829d1165a547d5cc47bc86
Blazxex <82823435+blazxex@users.noreply.github.com>
2024-01-18 12:47:00 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,75 +1,55 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.select import Select
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions #check if that element present
+from selenium.webdriver.support.ui import Select
 from datetime import datetime, timedelta
+import os
 
+def ItRenewer(Username,Password,Product):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
 
-
-def renew(username, password):
-    
-    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-
-    chrome_options = Options()
-    options = [
-        "--headless",
-        "--disable-gpu",
-        "--window-size=1920,1200",
-        "--ignore-certificate-errors",
-        "--disable-extensions",
-        "--no-sandbox",
-        "--disable-dev-shm-usage"
-    ]
-    for option in options:
-        chrome_options.add_argument(option)
-
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    driver = webdriver.Chrome(options=options)  
 
     driver.get('https://licenseportal.it.chula.ac.th/')
 
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.XPATH, '//input')))
+    wait.until(expected_conditions.presence_of_element_located((By.XPATH, '//input')))
+    print(driver.title)
+
 
     username_input = driver.find_element(By.ID, 'UserName')
-    username_input.send_keys(username)
+    username_input.send_keys(Username)
 
     password_input = driver.find_element(By.ID, 'Password')
-    password_input.send_keys(password)
+    password_input.send_keys(Password)
 
     signin_button = driver.find_element(By.XPATH, '//button')
     signin_button.click()
     
     driver.get('https://licenseportal.it.chula.ac.th/Home/Borrow')
+    
+    dropdown = Select(driver.find_element(By.ID,'ProgramLicenseID'))
+    dropdown.select_by_visible_text(Product)  
 
-    
-    
-    wait.until(EC.presence_of_all_elements_located((By.ID, 'ExpiryDateStr')))
+    wait.until(expected_conditions .presence_of_all_elements_located((By.ID, 'ExpiryDateStr')))
     
     expiry_date_input = driver.find_element(By.ID, 'ExpiryDateStr')
     week = datetime.now() + timedelta(days=7)
     driver.execute_script("arguments[0].value = arguments[1];", expiry_date_input, week.strftime('%d/%m/%Y'))
 
-
-    select_element = driver.find_element(By.ID, 'ProgramLicenseID')
-    select = Select(select_element)
-    select.select_by_value('5')
-
     save_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
     save_button.click()
-        
-    driver.quit()
-    
-    return True    
+    print(driver.title)
 
-import os
-USERNAME = os.environ['USERNAME']
-PASSWORD = os.environ['PASSWORD']
-renew(USERNAME, PASSWORD)
+    driver.quit()
+
+
+Username = os.environ['USERNAME']
+Password = os.environ['PASSWORD']
+Product = 'Adobe สำหรับนิสิต'
+ItRenewer(Username,Password,Product)
